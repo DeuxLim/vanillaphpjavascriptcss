411ef95ec8ea444b9499f14eca74f94eacbc96d0
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-07 22:57:54 +0800

    task model

--- public/js/model/Task.js
+++ public/js/model/Task.js
@@ -1,6 +1,10 @@
 export default class Task {
-    constructor() 
+    constructor(data = {}) 
     {
-
+        this.task_title = data.task_title || "";
+        this.task_description = data.task_description || "";
+        this.task_priority = data.task_priority || "";
+        this.task_due = data.task_due || "";
+        this.task_completed = data.task_completed || 0;
     }
 }
