68cfd75d75afe8d5577f6446a55b34db6af6498c
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-07 21:58:40 +0800

    updated taskApi

--- public/js/api/TaskAPI.js
+++ public/js/api/TaskAPI.js
@@ -4,30 +4,37 @@ export default class TaskAPI {
 
     }
 
+    async request(uri, method = "GET", bodyData = null, options = {}) {
+        const config = {
+            method,
+            headers: {
+                "Content-Type": "application/json",
+                ...options.headers
+            },
+            ...options
+        };
+
+        // only attach body for methods that allow it
+        if (bodyData && !["GET", "HEAD"].includes(method.toUpperCase())) {
+            config.body = JSON.stringify(bodyData);
+        }
+
+        const response = await fetch(uri, config);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        return await response.json();
+    }
+
     async getTasks()
     {
-        const response = await fetch("/tasks");
-        const data = await response.json();
-        return data;
+        return this.request("/tasks");
     }
 
-    async getTask()
+    async createTask(data)
     {
-
-    }
-
-    async createTask()
-    {
-
-    }
-
-    async updateTask()
-    {
-
-    }
-
-    async deleteTask()
-    {
-
+        return this.request("/tasks", "POST", data);
     }
 }
